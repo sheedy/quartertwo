12842e197b25dfedfedb2c59854fe72e4f11d748
Michael Sheedy <git@michaelsheedy.com>
2016-02-24 01:20:54 -0500

    publish: Tweaked animations
    generated from commit 8dd6d97579b126d1e61df005b61f3188797722f2

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@
  * quartertwo - v0.0.1
  * 2016-02-24
 **/
-var mark=document.getElementById("mark"),shop=document.getElementById("shop-now");new Vivus("six-logo",{type:"scenario",duration:300,pathTimingFunction:Vivus.EASE_OUT},function(a){a.el.classList.add("finished"),setTimeout(function(){mark.className+=" finished"},1e3),setTimeout(function(){shop.className+=" finished"},2e3)});
+var mark=document.getElementById("mark"),shop=document.getElementById("shop-now");new Vivus("six-logo",{type:"scenario",duration:300,pathTimingFunction:Vivus.EASE_OUT},function(a){a.el.classList.add("finished"),mark.className+=" finished",setTimeout(function(){shop.className+=" finished"},500)});
